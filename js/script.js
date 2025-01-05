c2bd70ed20aac9f36ca3b1e5e55f7f7383a31b90
Ed <mail@544062.ru>
2025-01-05 15:41:01 +0300

    feat: refactor

--- js/script.js
+++ js/script.js
@@ -4,173 +4,198 @@ let SPEED_LIMIT = 55; // км/ч
 let GEO_TIMEOUT = 5000; // 5 секунд
 let watchId;
 
+const statusElement = document.getElementById('status');
+const alarm = document.getElementById('alarmSound');
+
+const modal = document.getElementById('settings-modal');
+
+const speedLimit = document.getElementById('speedLimit');
+const geoTimeout = document.getElementById('geoTimeout');
+const playSound = document.getElementById('playSound');
+
+const speedLimitValue = document.getElementById('speedLimitValue');
+const geoTimeoutValue = document.getElementById('geoTimeoutValue');
+
 async function requestWakeLock() {
-    if ('wakeLock' in navigator) {
-        try {
-            wakeLock = await navigator.wakeLock.request('screen');
-            updateStatus('Wake Lock активирован');
-        } catch (err) {
-            updateStatus('Ошибка активации Wake Lock: ' + err);
-        }
-    } else {
-        updateStatus('Wake Lock API не поддерживается');
+  if ('wakeLock' in navigator) {
+    try {
+      wakeLock = await navigator.wakeLock.request('screen');
+      updateStatus('Блокировка экрана активирована');
+    } catch (err) {
+      updateStatus('Ошибка активации блокировки экрана: ' + err);
     }
+  } else {
+    updateStatus('Wake Lock API не поддерживается');
+  }
 }
 
 function updateSpeed(speed) {
-    const speedElement = document.getElementById('speed');
-    if (speedElement) {
-        const roundedSpeed = Math.round(speed);
-        speedElement.textContent = `${roundedSpeed} км/ч`;
+  const speedElement = document.getElementById('speed');
+  if (speedElement) {
+    const roundedSpeed = Math.round(speed);
+    speedElement.textContent = `${roundedSpeed} км/ч`;
 
-        const isOverSpeedLimit = roundedSpeed >= SPEED_LIMIT;
-        document.body.style.backgroundColor = isOverSpeedLimit ? 'red' : 'green';
+    const isOverSpeedLimit = roundedSpeed >= SPEED_LIMIT;
+    document.body.style.backgroundColor = isOverSpeedLimit ? 'red' : 'green';
 
-        if (isOverSpeedLimit && isSoundEnabled) {
-            playAlarm();
-        } else {
-            stopAlarm();
-        }
+    if (isOverSpeedLimit && isSoundEnabled) {
+      playAlarm();
+    } else {
+      stopAlarm();
     }
+  }
 }
 
 function updateStatus(message) {
-    const statusElement = document.getElementById('status');
-    if (statusElement) {
-        statusElement.textContent = message;
-    }
+  statusElement.textContent = message;
 }
 
 function handlePosition(position) {
-    const speed = position.coords.speed;
-    
-    if (speed !== null) {
-        // Преобразуем скорость из м/с в км/ч
-        const speedKMH = speed * 3.6;
-        updateSpeed(speedKMH);
-    } else {
-        updateStatus("Данные о скорости недоступны");
-    }
+  const speed = position.coords.speed;
+
+  if (speed !== null) {
+    // Преобразуем скорость из м/с в км/ч
+    const speedKMH = speed * 3.6;
+    updateSpeed(speedKMH);
+  } else {
+    updateStatus('Данные о скорости недоступны');
+  }
 }
 
 function handleError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            updateStatus("Пользователь отказал в доступе к геолокации.");
-            break;
-        case error.POSITION_UNAVAILABLE:
-            updateStatus("Информация о местоположении недоступна.");
-            break;
-        case error.TIMEOUT:
-            updateStatus("Истекло время ожидания запроса на определение местоположения.");
-            break;
-        case error.UNKNOWN_ERROR:
-            updateStatus("Произошла неизвестная ошибка.");
-            break;
-    }
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      updateStatus('Пользователь отказал в доступе к геолокации.');
+      break;
+    case error.POSITION_UNAVAILABLE:
+      updateStatus('Информация о местоположении недоступна.');
+      break;
+    case error.TIMEOUT:
+      updateStatus(
+        'Истекло время ожидания запроса на определение местоположения.'
+      );
+      break;
+    case error.UNKNOWN_ERROR:
+      updateStatus('Произошла неизвестная ошибка.');
+      break;
+  }
 }
 
 function playAlarm() {
-    const alarm = document.getElementById('alarmSound');
-    if (alarm) {
-        alarm.play().catch(e => console.error("Ошибка воспроизведения звука:", e));
-    }
+  alarm.play().catch((e) => console.error('Ошибка воспроизведения звука:', e));
 }
 
 function stopAlarm() {
-    const alarm = document.getElementById('alarmSound');
-    if (alarm) {
-        alarm.pause();
-        alarm.currentTime = 0;
-    }
+  alarm.pause();
+  alarm.currentTime = 0;
 }
 
 function toggleSound() {
-    isSoundEnabled = !isSoundEnabled;
-    const toggleButton = document.getElementById('toggleSound');
-    if (toggleButton) {
-        const icon = toggleButton.querySelector('i');
-        if (icon) {
-            icon.className = isSoundEnabled ? 'fas fa-volume-up' : 'fas fa-volume-mute';
-        }
+  isSoundEnabled = !isSoundEnabled;
+  const toggleButton = document.getElementById('toggleSound');
+  if (toggleButton) {
+    const icon = toggleButton.querySelector('i');
+    if (icon) {
+      icon.className = isSoundEnabled
+        ? 'fas fa-volume-up'
+        : 'fas fa-volume-mute';
     }
-    updateStatus(isSoundEnabled ? 'Звук включен' : 'Звук выключен');
+  }
+  updateStatus(isSoundEnabled ? 'Звук включен' : 'Звук выключен');
 }
 
 function openSettings() {
-    const modal = document.getElementById('settings-modal');
-    if (modal) {
-        modal.style.display = 'block';
-    }
+  modal.style.display = 'block';
+  updateSettingsDisplay();
 }
 
 function closeSettings() {
-    const modal = document.getElementById('settings-modal');
-    if (modal) {
-        modal.style.display = 'none';
-    }
+  modal.style.display = 'none';
 }
 
 function updateSettingsDisplay() {
-    document.getElementById('speedLimitValue').textContent = SPEED_LIMIT;
-    document.getElementById('geoTimeoutValue').textContent = GEO_TIMEOUT / 1000;
-    document.getElementById('playSound').checked = isSoundEnabled;
+  speedLimitValue.textContent = SPEED_LIMIT + ' км/ч';
+  geoTimeoutValue.textContent = GEO_TIMEOUT / 1000 + ' сек';
+
+  speedLimit.value = SPEED_LIMIT;
+  speedLimit.style.setProperty('--value', speedLimit.value / speedLimit.max);
+
+  geoTimeout.value = GEO_TIMEOUT / 1000;
+  geoTimeout.style.setProperty(
+    '--value',
+    (geoTimeout.value - geoTimeout.min) / (geoTimeout.max - geoTimeout.min)
+  );
+
+  playSound.checked = isSoundEnabled;
 }
 
 function saveSettings(e) {
-    e.preventDefault();
-    SPEED_LIMIT = parseInt(document.getElementById('speedLimit').value);
-    GEO_TIMEOUT = parseInt(document.getElementById('geoTimeout').value) * 1000;
-    isSoundEnabled = document.getElementById('playSound').checked;
-    updateSettingsDisplay();
-    closeSettings();
-    restartGeolocation();
+  e.preventDefault();
+  SPEED_LIMIT = parseInt(speedLimit.value);
+  GEO_TIMEOUT = parseInt(geoTimeout.value) * 1000;
+  isSoundEnabled = playSound.checked;
+  updateSettingsDisplay();
+  closeSettings();
+  restartGeolocation();
 }
 
 function restartGeolocation() {
-    if ("geolocation" in navigator) {
-        navigator.geolocation.clearWatch(watchId);
-        watchId = navigator.geolocation.watchPosition(handlePosition, handleError, {
-            enableHighAccuracy: true,
-            timeout: GEO_TIMEOUT,
-            maximumAge: 0
-        });
-    }
+  if ('geolocation' in navigator) {
+    navigator.geolocation.clearWatch(watchId);
+    watchId = navigator.geolocation.watchPosition(handlePosition, handleError, {
+      enableHighAccuracy: true,
+      timeout: GEO_TIMEOUT,
+      maximumAge: 0,
+    });
+  }
 }
 
 function init() {
-    if ("geolocation" in navigator) {
-        watchId = navigator.geolocation.watchPosition(handlePosition, handleError, {
-            enableHighAccuracy: true,
-            timeout: GEO_TIMEOUT,
-            maximumAge: 0
-        });
-        requestWakeLock();
-    } else {
-        updateStatus("Геолокация не поддерживается вашим браузером.");
-    }
+  if ('geolocation' in navigator) {
+    watchId = navigator.geolocation.watchPosition(handlePosition, handleError, {
+      enableHighAccuracy: true,
+      timeout: GEO_TIMEOUT,
+      maximumAge: 0,
+    });
+    requestWakeLock();
+  } else {
+    updateStatus('Геолокация не поддерживается вашим браузером.');
+  }
 
-    const toggleButton = document.getElementById('toggleSound');
-    if (toggleButton) {
-        toggleButton.addEventListener('click', toggleSound);
-    }
+  const toggleButton = document.getElementById('toggleSound');
+  if (toggleButton) {
+    toggleButton.addEventListener('click', toggleSound);
+  }
 
-    const openSettingsButton = document.getElementById('openSettings');
-    if (openSettingsButton) {
-        openSettingsButton.addEventListener('click', openSettings);
-    }
+  const openSettingsButton = document.getElementById('openSettings');
+  if (openSettingsButton) {
+    openSettingsButton.addEventListener('click', openSettings);
+  }
 
-    const closeSettingsButton = document.getElementById('closeSettings');
-    if (closeSettingsButton) {
-        closeSettingsButton.addEventListener('click', closeSettings);
-    }
+  const closeSettingsButton = document.getElementById('closeSettings');
+  if (closeSettingsButton) {
+    closeSettingsButton.addEventListener('click', closeSettings);
+  }
 
-    const settingsForm = document.getElementById('settings-form');
-    if (settingsForm) {
-        settingsForm.addEventListener('submit', saveSettings);
-    }
+  const settingsForm = document.getElementById('settings-form');
+  if (settingsForm) {
+    settingsForm.addEventListener('submit', saveSettings);
+  }
 
-    updateSettingsDisplay();
+  speedLimit.addEventListener('input', function () {
+    speedLimitValue.textContent = parseInt(this.value) + ' км/ч';
+    this.style.setProperty('--value', this.value / this.max);
+  });
+
+  geoTimeout.addEventListener('input', function () {
+    geoTimeoutValue.textContent = parseInt(this.value) + ' сек';
+    this.style.setProperty(
+      '--value',
+      (this.value - this.min) / (this.max - this.min)
+    );
+  });
+
+  updateSettingsDisplay();
 }
 
 // Запуск приложения после загрузки DOM
@@ -178,8 +203,7 @@ document.addEventListener('DOMContentLoaded', init);
 
 // Обработка событий видимости страницы для управления Wake Lock
 document.addEventListener('visibilitychange', async () => {
-    if (wakeLock !== null && document.visibilityState === 'visible') {
-        await requestWakeLock();
-    }
+  if (wakeLock !== null && document.visibilityState === 'visible') {
+    await requestWakeLock();
+  }
 });
-
